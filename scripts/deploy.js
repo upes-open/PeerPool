2a47be21ea52043be629dd0a3d9fded4da10bcfc
Pawan <pawanbajaj2012@gmail.com>
2022-09-28 19:01:32 +0530

    post issues

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,31 +1,41 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const {ethers, run, network} = require("hardhat")
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const carpoolFactory = await ethers.getContractFactory("carpooling")
+  console.log("Deploying contract...")
+  const carpooling = await carpoolFactory.deploy()
+  await carpooling.deployed()
+  console.log(`Deployed contract to: ${carpooling.address}`)
+  if(network.config.chainId == 5 && process.env.ETHERSCAN_API_KEY) {
+    await simpleStorage.deployTransaction.wait(6)
+    await verify(simpleStorage.address, [])
+  }
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+async function verify(contractAddress, args) {
+  console.log("verifying contract")
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      contructorArguments: args,
+    })
+  }
+  catch (e) {
+    if(e.message.toLowerCase().includes("already verified")) {
+      console.log("Already Verified")
+    }
+    else {
+      console.log(e)
+    }
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+
