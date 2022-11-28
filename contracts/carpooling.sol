333942813935311dc5cca7e6aa88a9ae18c65340
Pawan <pawanbajaj2012@gmail.com>
2022-11-28 23:27:10 +0530

    contract updated

--- contracts/carpooling.sol
+++ contracts/carpooling.sol
@@ -1,14 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// the contract needs to be updated with events for transaction data
-
 contract carpooling {
-    // struct human {
-    //     string name;
-    //     uint8 age;
-    //     string gender;
-    // }
+    struct human {
+        string name;
+        uint8 age;
+        bool gender;    // 0 for female, 1 for male
+    }
     struct ride {
         uint rideId;
         string origin;
@@ -20,34 +18,45 @@ contract carpooling {
 
     mapping (uint => address) public rideowner;
     mapping (uint => mapping(uint => address)) public rideToRider;
-
     uint8 ridecount = 0;
     ride[] public rides;
+    human[] public person;
+    mapping (address => human) public addressDetails;
     // ride[] public searchRides;
 
+    event rideCreated(
+        uint rideId,
+        string origin,
+        string destination,
+        uint departuretime,
+        uint fare,
+        uint seats        
+    );
+    event rideBooked(
+        uint rideId,
+        uint seats,
+        address passenger
+    );
 
-    function createride(string memory _origin, string memory _destination, uint _departuretime, uint8 _seats, uint8 _fare) public {
-        rides.push(ride(ridecount, _origin, _destination, _departuretime, _seats,_fare));
+    function newUser(string memory _name, uint8 _age, bool _gender) public {
+        person.push(human(_name, _age, _gender));
+        addressDetails[msg.sender].name = _name;
+        addressDetails[msg.sender].age = _age;
+        addressDetails[msg.sender].gender = _gender;
+    }
+
+
+    function createride(string memory _origin, string memory _destination, uint _departuretime, uint8 _fare, uint8 _seats) public {
+        rides.push(ride(ridecount, _origin, _destination, _departuretime, _fare, _seats));
         rideowner[ridecount] = msg.sender;
+        emit rideCreated(ridecount, _origin, _destination, _departuretime, _fare, _seats);
         ridecount++;
     }  
-
-
-    function searchride(string memory __origin, string memory __destination)public view returns(ride[] memory searchRides) {
-        for (uint i = 0; i < ridecount; i = i+1) {
-            ride storage checkRide;
-            if (keccak256(abi.encodePacked(rides[i].origin)) == keccak256(abi.encodePacked(__origin)) && keccak256(abi.encodePacked(rides[i].destination)) == keccak256(abi.encodePacked(__destination)) && rides[i].seats > 0) { 
-                checkRide = rides[i];
-                searchRides[i] = checkRide;
-            }
-            
-        }
-        return searchRides;        
-    }
 
     function bookRide(uint rideId) public {
         rideToRider[rideId][rides[rideId].seats] = msg.sender;
         rides[rideId].seats -= 1;
+        emit rideBooked(rideId, rides[rideId].seats, msg.sender);
     }
 
 }
