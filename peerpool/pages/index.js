ca6330d6e468c93a6af64c83a184bb4e8ea45971
Pawan <pawanbajaj2012@gmail.com>
2023-01-30 13:00:45 +0530

    removed useless imports

--- peerpool/pages/index.js
+++ peerpool/pages/index.js
@@ -4,9 +4,7 @@ import Image from 'next/image'
 
 import styles from "../styles/Home.module.css";
 import Web3Modal from "web3modal";
-import {providers, Contract} from "ethers";
 import { useEffect,useRef, useState } from "react";
-import {CONTRACT_ADDRESS, CONTRACT_ABI} from "../constants";
 import { ethers } from "ethers";
 
 
